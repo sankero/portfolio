f17646296544e7b16852b01d29512fac0ba767fb
tagawa <sankero@gmail.com>
2021-08-28 22:08:12 +0900

    add google analytics

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,15 @@
 import Layout from './Layout.vue'
+import { watch } from 'vue'
 
 export default {
   Layout,
   NotFound: () => 'custom 404',
-  enhanceApp({ app, router, siteData }) {}
+  enhanceApp({ app, router, siteData }) {
+    // Google Analytics
+    if (globalThis && globalThis.gtag) {
+      watch(router.route, () => {
+        gtag('config', window.GA_MEASUREMENT_ID, {'page_path': router.route.path });
+      })
+    }
+  }
 }
