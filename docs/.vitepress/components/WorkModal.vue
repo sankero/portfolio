1f42dc6146e5f6d2d8a51a49292b4a645dcca2ff
tagawa <sankero@gmail.com>
2021-09-05 23:59:53 +0900

    update modal transition

--- docs/.vitepress/components/WorkModal.vue
+++ docs/.vitepress/components/WorkModal.vue
@@ -5,10 +5,18 @@
       href="/"
     />
     <nav class="modal-nav">
-      <a v-if="pageLink.prev" :href="pageLink.prev" class="modal-nav-prev">
+      <a
+        v-if="pageLink.prev"
+        :href="pageLink.prev"
+        class="modal-nav-prev">
+
         <arrowDownIcon class="arrow" />
       </a>
-      <a v-if="pageLink.next" :href="pageLink.next" class="modal-nav-next">
+      <a
+        v-if="pageLink.next"
+        :href="pageLink.next"
+        class="modal-nav-next">
+
         <arrowDownIcon class="arrow" />
       </a>
     </nav>
@@ -16,6 +24,7 @@
       <article
         v-show="worksShowFlg"
         class="modal-innr md"
+        :class="transitionClass"
       >
         <header class="md-header">
           <h1 class="md-title">
@@ -71,7 +80,7 @@
 </template>
 
 <script lang="ts">
-import { inject } from 'vue'
+import { ref, inject } from 'vue'
 import { store, storeKey } from '../store/store.ts'
 import { useData } from 'vitepress'
 import arrowDownIcon from './icon/arrowDown.vue'
@@ -86,235 +95,243 @@ export default {
       default() {
         return {
           next: '',
-          prev: ''
+          prev: '',
         }
-      }
-    }
+      },
+    },
   },
   setup() {
     const { worksShowFlg } = inject(storeKey) as store
     const { page } = useData()
+    const transitionClass = ref('open')
+
     return {
       // store
       worksShowFlg,
       // this
       page,
+      transitionClass,
     }
   },
 }
 </script>
 
-<style lang="scss">
-  .modal {
-    position: fixed;
+<style lang="stylus">
+.modal {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  min-height: var(--windowHeight, 100vh);
+  top: 0;
+  left: 0;
+  background: rgba(0,0,0,0.7);
+  overflow: scroll;
+  font-size: 20px;
+  z-index: 5;
+  &-bg {
     width: 100%;
     height: 100%;
-    min-height: var(--windowHeight, 100vh);
-    top: 0;
-    left: 0;
-    background: rgba(0,0,0,0.7);
-    overflow: scroll;
-    font-size: 20px;
-    z-index: 5;
-    &-bg {
-      width: 100%;
-      height: 100%;
-      position: fixed;
-      z-index: -1;
-    }
-    &-innr {
-      width: 96%;
-      max-width: 960px;
-      min-height: 420px;
-      margin: 4em auto 2vw;
-      padding: 2em 1em;
-      box-sizing: border-box;
-      z-index: 1;
-      box-shadow: 30px 30px 20px #222;
-      background: rgba(255,255,255,0.8);
-      border-radius: 10px;
-      border: 1px solid rgba(255,255,255,0.2);
-      backdrop-filter: blur(5px);
-      animation: slide-skew 0.4s cubic-bezier(0.25, 1, 0.5, 1) 1 forwards;
-      // PC
-      @media (min-aspect-ratio: 1 / 1) {
-        width: 90%;
-        margin: 5vh auto;
-        padding: 5em 3em 3em;
-      }
-    }
-    &-nav {
-      &-next,
-      &-prev {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 3vw;
-        min-width: 30px;
-        position: fixed;
-        top: 90vh;
-        z-index: 1;
-        background: rgba(0,0,0,0.4);
-        border-radius: 50%;
-        padding: 2vw;
-        opacity: 0.6;
-        transition-duration: 0.1s;
-        &:hover {
-          opacity: 1;
-        }
-      }
-      &-next {
-        transform: rotate(-90deg);
-        right: 2vw;
-      }
-      &-prev {
-        transform: rotate(90deg);
-        left: 2vw;
-      }
-      svg {
-        fill: #FFF;
-      }
-    }
-    &-close {
-      padding: 0.2em 0.8em;
-      border-radius: 50px;
-      background: #c3c3c3;
-      box-shadow: 6px 6px 6px #999, -3px -3px 6px #eee;
-      display: inline-flex;
-      justify-content: center;
-      align-items: center;
-      text-decoration: none;
-      color: #000;
-      margin-top: 2em;
-      user-select: none;
-      &:active {
-        box-shadow: inset 3px 3px 4px #999999, inset -3px -3px 4px #cfcfcf;
-      }
+    position: fixed;
+    z-index: -1;
+  }
+  &-innr {
+    width: 96%;
+    max-width: 960px;
+    min-height: 420px;
+    margin: 4em auto 2vw;
+    padding: 2em 1em;
+    box-sizing: border-box;
+    z-index: 1;
+    box-shadow: 30px 30px 20px #222;
+    background: rgba(255,255,255,0.8);
+    border-radius: 10px;
+    border: 1px solid rgba(255,255,255,0.2);
+    backdrop-filter: blur(5px);
+    // transition
+    opacity: 1;
+    animation: modal-open-anime;
+    animation-timing-function: ease-out;
+    animation-duration: 0.3s;
+    transition: all 5s ease;
+    transition-duration: 2s;
+    // PC
+    @media (min-aspect-ratio: 1 / 1) {
+      width: 90%;
+      margin: 5vh auto;
+      padding: 5em 3em 3em;
     }
   }
-  /* markdown style */
-  .md {
-    &-header {
-      margin-bottom: 1em;
-      text-align: center;
-      // PC
-      @media (min-aspect-ratio: 1 / 1) {
-        margin-bottom: 3em;
-      }
-    }
-    &-url {
-      color: #444;
-      display: block;
-      font-size: 0.8em;
-      text-decoration: none;
-    }
-    &-title {
-      font-size: 1.2em;
-      font-weight: 200;
-      margin: 0;
-      @media (min-aspect-ratio: 1 / 1) {
-        font-size: 1.6em;
-      }
-    }
-    &-tags {
+  &-nav {
+    &-next,
+    &-prev {
       display: flex;
-      flex-wrap: wrap;
-      list-style: none;
+      align-items: center;
       justify-content: center;
-      margin: 0;
-      padding-left: 0;
-      li {
-        margin: 0.2em;
-        padding: 0.2em 0.5em;
-        background: rgba(255, 255, 255, 0.1);
-        border-radius: 4px;
-        font-size: 0.7em;
-        color: #666;
+      width: 3vw;
+      min-width: 30px;
+      position: fixed;
+      top: 90vh;
+      z-index: 1;
+      background: rgba(0,0,0,0.4);
+      border-radius: 50%;
+      padding: 2vw;
+      opacity: 0.6;
+      transition-duration: 0.1s;
+      &:hover {
+        opacity: 1;
       }
     }
-    &-mv {
+    &-next {
+      transform: rotate(-90deg);
+      right: 2vw;
+    }
+    &-prev {
+      transform: rotate(90deg);
+      left: 2vw;
+    }
+    svg {
+      fill: #FFF;
+    }
+  }
+  &-close {
+    padding: 0.2em 0.8em;
+    border-radius: 50px;
+    background: #c3c3c3;
+    box-shadow: 6px 6px 6px #999, -3px -3px 6px #eee;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    color: #000;
+    margin-top: 2em;
+    user-select: none;
+    &:active {
+      box-shadow: inset 3px 3px 4px #999999, inset -3px -3px 4px #cfcfcf;
+    }
+  }
+}
+/* markdown style */
+.md {
+  &-header {
+    margin-bottom: 1em;
+    text-align: center;
+    // PC
+    @media (min-aspect-ratio: 1 / 1) {
+      margin-bottom: 3em;
+    }
+  }
+  &-url {
+    color: #444;
+    display: block;
+    font-size: 0.8em;
+    text-decoration: none;
+  }
+  &-title {
+    font-size: 1.2em;
+    font-weight: 200;
+    margin: 0;
+    @media (min-aspect-ratio: 1 / 1) {
+      font-size: 1.6em;
+    }
+  }
+  &-tags {
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    justify-content: center;
+    margin: 0;
+    padding-left: 0;
+    li {
+      margin: 0.2em;
+      padding: 0.2em 0.5em;
+      background: rgba(255, 255, 255, 0.1);
+      border-radius: 4px;
+      font-size: 0.7em;
+      color: #666;
+    }
+  }
+  &-mv {
+    text-align: center;
+    margin-bottom: 1em;
+    @media (min-aspect-ratio: 1 / 1) {
+      margin-bottom: 3em;
+    }
+    &-img {
+      max-width: 100%;
+    }
+  }
+  &-content {
+    line-height: 1.5;
+    // Mobile
+    @media (max-aspect-ratio: 1 / 1) {
+      font-size: 3vw;
+    }
+    // PC
+    @media (min-aspect-ratio: 1 / 1) {
+      line-height: 1.8;
+    }
+    h2 {
       text-align: center;
-      margin-bottom: 1em;
-      @media (min-aspect-ratio: 1 / 1) {
-        margin-bottom: 3em;
-      }
-      &-img {
-        max-width: 100%;
-      }
+      font-weight: 200;
+      margin: 2em auto 1em;
+      font-size: 1.2em;
     }
-    &-content {
-      line-height: 1.5;
-      // Mobile
-      @media (max-aspect-ratio: 1 / 1) {
-        font-size: 3vw;
-      }
-      // PC
-      @media (min-aspect-ratio: 1 / 1) {
-        line-height: 1.8;
-      }
-      h2 {
-        text-align: center;
-        font-weight: 200;
-        margin: 2em auto 1em;
-        font-size: 1.2em;
-      }
-      h3 {
-        font-size: 1em;
-        font-weight: 200;
-        border-bottom: 1px dashed #999;
-        margin: 1.5em auto 0.2em;
-      }
-      img {
-        max-width: 100%;
-      }
-      // 引用
-      blockquote {
-        font-size: 0.8em;
-        position: relative;
-        padding: 0.2em 1em;
-        box-sizing: border-box;
-        font-style: italic;
-        background: rgba(0,0,0, 0.1);
-        &:before {
-          color: #cfcfcf;
-          font-size: 2em;
-          font-weight: bold;
-          color: #CCC;
-          line-height: 0.6;
-          content: open-quote;
-          display: block;
-          transform: translateY(0.4em);
-        }
-        p {
-          padding-left: 1em;
-          line-height: 1.2;
-        }
-        cite {
-          display: block;
-          text-align: right;
-        }
-      }
+    h3 {
+      font-size: 1em;
+      font-weight: 200;
+      border-bottom: 1px dashed #999;
+      margin: 1.5em auto 0.2em;
     }
-    .header-anchor {
-      display: none;
+    img {
+      max-width: 100%;
     }
-    &-footer {
-      text-align: center;
-      &-headline {
-        font-weight: 200;
-        margin: 1em 0 0.5em;
+    // 引用
+    blockquote {
+      font-size: 0.8em;
+      position: relative;
+      padding: 0.2em 1em;
+      box-sizing: border-box;
+      font-style: italic;
+      background: rgba(0,0,0, 0.1);
+      &:before {
+        color: #cfcfcf;
+        font-size: 2em;
+        font-weight: bold;
+        color: #CCC;
+        line-height: 0.6;
+        content: open-quote;
+        display: block;
+        transform: translateY(0.4em);
+      }
+      p {
+        padding-left: 1em;
+        line-height: 1.2;
+      }
+      cite {
+        display: block;
+        text-align: right;
       }
     }
   }
-
-  .modal-innr-enter-active,
-  .modal-innr-leave-active {
-    // transition-delay: 0.8s;
-    transition: opacity 0.5s ease;
+  .header-anchor {
+    display: none;
   }
-
-  .modal-innr-enter-from,
-  .modal-innr-leave-to {
+  &-footer {
+    text-align: center;
+    &-headline {
+      font-weight: 200;
+      margin: 1em 0 0.5em;
+    }
+  }
+}
+@keyframes modal-open-anime {
+  0% {
+    transform: translateY(10vh);
     opacity: 0;
   }
+  100%{
+    transform: translateY(0vh);
+    opacity: 1;
+  }
+}
 </style>
